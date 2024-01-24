8c59b61de0df7fb7698dd6ee4846dc8febfd7fed
HasanKoc33 <developer.hasankoc+github@gmail.com>
2024-01-25 00:22:11 +0300

    .

--- lib/src/menu_title.dart
+++ lib/src/menu_title.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
-
 class MenuTitle extends StatefulWidget {
   /// The children of this widget.
   final List<Widget> children;
+
   /// The title of this widget.
   final String title;
+
+  /// active icon
+  final Icon? activeIcon;
+
+  /// inactive icon
+  final Icon? inactiveIcon;
+
+  /// açık mı başlasın
+  final bool? isOpenStart;
 
   const MenuTitle({
     super.key,
     required this.title,
     required this.children,
+    this.activeIcon,
+    this.inactiveIcon,
+    this.isOpenStart,
   });
 
   @override
@@ -21,26 +33,32 @@ class _MenuTitleState extends State<MenuTitle> {
   bool isCollapsed = false;
 
   @override
+  void initState() {
+    super.initState();
+    setState(() {
+      isCollapsed = widget.isOpenStart ?? false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    debugPrint('isCollapsed: $isCollapsed');
     return ExpansionTile(
       iconColor: Theme.of(context).colorScheme.secondary,
       collapsedIconColor: Theme.of(context).colorScheme.background,
       onExpansionChanged: (value) => setState(() {
         isCollapsed = value;
       }),
-      leading: Icon(
-        isCollapsed ? Icons.menu : Icons.menu_open,
-        color:  Theme.of(context).colorScheme.secondary,
-      ),
+      maintainState: isCollapsed,
+      leading: isCollapsed
+          ? widget.activeIcon ?? const Icon(Icons.menu)
+          : widget.activeIcon ?? const Icon(Icons.menu_open),
       title: Text(
         widget.title,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.secondary,
-        ),
+        style: Theme.of(context).textTheme.titleMedium
       ),
       controlAffinity: ListTileControlAffinity.leading,
       children: widget.children,
     );
   }
 }
-
